384d76d300cd27525be52c8eb1c5b09e26d7d676
Andrew Cannan <andrewmcannan@gmail.com>
2024-06-20 21:13:35 +0100

    install bootstrap and add neccessary imports for bootstrap and react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
     const [text, setText] = useState("");
