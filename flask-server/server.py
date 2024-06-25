3d7f47f82b5b3e0b7ed25b5fbd7d63a79a0f51d3
Andrew Cannan <andrewmcannan@gmail.com>
2024-06-25 20:43:53 +0100

    install sqlalchemy, dotenv and psycopg2. setup postgres connection

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"postgresql://{os.getenv('PGUSER')}:{os.getenv('PGPASSWORD')}"
+    f"@{os.getenv('PGHOST')}:{os.getenv('PGPORT')}/{os.getenv('PGDATABASE')}"
+)
 
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
