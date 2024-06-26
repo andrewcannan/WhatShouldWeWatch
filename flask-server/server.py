439225043c753d15502275f829fef52144669a95
Andrew Cannan <andrewmcannan@gmail.com>
2024-06-26 20:16:06 +0100

    install flask-migrate and import to app

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 
@@ -8,6 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"postgresql://{os.getenv('PGUSER')}:{os.getenv('PGPASSWORD')}"
