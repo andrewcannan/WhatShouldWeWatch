c20721bb708ffef1dc9703c1b7ce3e19565610f9
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-01 22:28:35 +0100

    handle submit of formData

--- client/src/components/forms/RegisterForm.js
+++ client/src/components/forms/RegisterForm.js
@@ -6,16 +6,50 @@ const RegisterForm = () => {
         username: '',
         password: '',
         confirmPassword: ''
-    })
+    });
 
     const handleChange = (e) => {
-        const {id, value} = e.target
+        const { id, value } = e.target
         setFormData({...formData, [id]: value})
+    };
 
-    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { username, password, confirmPasword } = formData;
+
+        if (password !== confirmPasword) {
+            // change style of input, add warning to user, remove alert
+            alert('Passwords do not match');
+            return;
+        }
+
+        const form = new FormData();
+        form.append('username', username);
+        form.append('pasword', password);
+
+        try {
+            const response = await fetch('/register', {
+                method: 'POST',
+                body: form
+            });
+
+            if (response.ok) {
+                // add success toast, remove console log, remove alert
+                const data = await response.json();
+                console.log('Registration Successful:', data);
+            } else {
+                // add error toast, remove console error, remove alert
+                const errorData = await response.json();
+                console.error('Registration Failed:', errorData)
+            }
+        } catch(error) {
+            // add unexpected error toast
+            console.error('An error occured:', error);
+        }
+    };
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="form-floating mb-3">
                 <input type="text" className="form-control" id="username" value={formData.username} onChange={handleChange} placeholder="Username" required></input>
                 <label for="username">Username</label>
@@ -30,7 +64,7 @@ const RegisterForm = () => {
             </div>
             <button type="submit" className="btn btn-danger mb-3">Register</button>
         </form>
-    )
-}
+    );
+};
 
 export default RegisterForm;
