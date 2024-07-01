743245bf777e44c622b8aa5b657dabe4bc20f3c2
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-01 22:02:23 +0100

    initialize state of formData and track changes

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const LoginForm = () => {
+    const [ formData, setFormdata ] = useState({
+        usename: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const {id, value} = e.target;
+        setFormdata({...formData, [id]: value});
+    }
+
     return (
         <form>
             <div className="form-floating mb-3">
-                <input type="text" className="form-control" id="floatingUsername" placeholder="Username"></input>
-                <label for="floatingUsername">Username</label>
+                <input type="text" className="form-control" id="username" value={formData.username} onChange={handleChange} placeholder="Username" required></input>
+                <label for="username">Username</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
-                <label for="floatingPassword">Password</label>
+                <input type="password" className="form-control" id="password" value={formData.password} placeholder="Password" required></input>
+                <label for="password">Password</label>
             </div>
             <button type="submit" className="btn btn-danger mb-3">Log In</button>
         </form>
