5e922ffed00fc3fe55946fe40621e31353c7a583
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-01 21:55:56 +0100

    initialize state of formData and track changes

--- client/src/components/forms/RegisterForm.js
+++ client/src/components/forms/RegisterForm.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const RegisterForm = () => {
+    const [ formData, setFormData ] = useState({
+        username: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleChange = (e) => {
+        const {id, value} = e.target
+        setFormData({...formData, [id]: value})
+
+    }
+
     return (
         <form>
             <div className="form-floating mb-3">
-                <input type="text" className="form-control" id="floatingUsername" placeholder="Username"></input>
-                <label for="floatingUsername">Username</label>
+                <input type="text" className="form-control" id="username" value={formData.username} onChange={handleChange} placeholder="Username" required></input>
+                <label for="username">Username</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
-                <label for="floatingPassword">Password</label>
+                <input type="password" className="form-control" id="password" value={formData.password} onChange={handleChange} placeholder="Password" required></input>
+                <label for="password">Password</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="password" className="form-control" id="floatingConfirmPassword" placeholder="Confirm Password"></input>
-                <label for="floatingConfirmPassword">Confirm Password</label>
+                <input type="password" className="form-control" id="confirmPassword" value={formData.confirmPassword} onChange={handleChange} placeholder="Confirm Password" required></input>
+                <label for="confirmPassword">Confirm Password</label>
             </div>
             <button type="submit" className="btn btn-danger mb-3">Register</button>
         </form>
