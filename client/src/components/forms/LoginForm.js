07e93bcface05da888afb312b00a1ceb93e4527a
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-01 21:15:54 +0100

    fix incorrect use of class, add bottom margins

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -4,15 +4,15 @@ import React from "react";
 const LoginForm = () => {
     return (
         <form>
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingUsername" placeholder="Username"></input>
+            <div className="form-floating mb-3">
+                <input type="text" className="form-control" id="floatingUsername" placeholder="Username"></input>
                 <label for="floatingUsername">Username</label>
             </div>
-                <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
+            <div className="form-floating mb-3">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
                 <label for="floatingPassword">Password</label>
             </div>
-            <button type="submit" class="btn btn-danger">Submit</button>
+            <button type="submit" className="btn btn-danger mb-3">Submit</button>
         </form>
     );
 }
