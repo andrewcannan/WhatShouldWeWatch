d1e5d011429e2f7cadc327882a1432caaf88bfb8
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-01 22:45:55 +0100

    handle submit of formData

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 const LoginForm = () => {
     const [ formData, setFormdata ] = useState({
-        usename: '',
+        username: '',
         password: ''
     })
 
@@ -12,8 +12,37 @@ const LoginForm = () => {
         setFormdata({...formData, [id]: value});
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const form = new FormData();
+        for (const key in formData) {
+            form.append(key, formData[key]);
+        }
+
+        try {
+            const response = await fetch('/login', {
+                method: 'POST',
+                body: form
+            });
+
+            if (response.ok) {
+                // add success toast, remove console log
+                const data = await response.json();
+                console.log('Login Successful:', data);
+            } else {
+                // add error toast, remove console error
+                const errorData = response.json();
+                console.error('Login Failed:', errorData)
+            }
+        } catch(error) {
+            // add unexpected error toast
+            console.error('An error occured:', error)
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="form-floating mb-3">
                 <input type="text" className="form-control" id="username" value={formData.username} onChange={handleChange} placeholder="Username" required></input>
                 <label for="username">Username</label>
