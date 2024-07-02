fcf5e87e162e13426459ed23f78e1bb011fad0db
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-02 22:39:27 +0100

    create register page componenet

---
+++ client/src/pages/Register/Register.js
@@ -0,0 +1,19 @@
+import React from "react";
+import RegisterForm from "../../components/forms/RegisterForm";
+import { Link } from 'react-router-dom';
+import './Register.css'
+
+
+const Register = () => {
+    return(
+        <div className="register">
+            <div className="form-wrapper">
+                <h1 className="mb-3">Create Account</h1>
+                <RegisterForm />
+                <p>Already a Member? <Link to='/login'>Login Here!</Link></p>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
