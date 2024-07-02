b2f7d9cf30a5563316ff9507625638b605a72857
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-02 19:55:18 +0100

    fix incorrect use of for keyword, add onChange to password input

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -45,11 +45,11 @@ const LoginForm = () => {
         <form onSubmit={handleSubmit}>
             <div className="form-floating mb-3">
                 <input type="text" className="form-control" id="username" value={formData.username} onChange={handleChange} placeholder="Username" required></input>
-                <label for="username">Username</label>
+                <label htmlFor="username">Username</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="password" className="form-control" id="password" value={formData.password} placeholder="Password" required></input>
-                <label for="password">Password</label>
+                <input type="password" className="form-control" id="password" value={formData.password} onChange={handleChange} placeholder="Password" required></input>
+                <label htmlFor="password">Password</label>
             </div>
             <button type="submit" className="btn btn-danger mb-3">Log In</button>
         </form>
