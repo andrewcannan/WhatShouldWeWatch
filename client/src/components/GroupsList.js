c2cbc53012875e5db1a5497eedef91123fb63e93
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-05 20:56:20 +0100

    add redirect and toast if user not logged i.e no user cookie

--- client/src/components/GroupsList.js
+++ client/src/components/GroupsList.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { getCookie } from './cookieUtil';
+import { useNavigate } from 'react-router-dom';
+import { showErrorToast } from './ToastHelper';
 
 
 const GroupsList = () => {
+    const navigate = useNavigate();
     const [ user, setUser ] = useState();
 
     useEffect(() => {
         const userCookie = getCookie('user');
-        setUser(userCookie);
-    }, []);
+        if (!userCookie) {
+            showErrorToast("Oops! Please sign in to continue.");
+            navigate('/login');
+        } else {
+            setUser(userCookie);
+        }
+    }, [navigate]);
 }
 
 export default GroupsList;
