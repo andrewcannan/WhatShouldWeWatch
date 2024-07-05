e979fd125ddb719bbaa809104e392bae965a7b1d
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-05 20:32:18 +0100

    create utility function for retrieving cookies

---
+++ client/src/components/cookieUtil.js
@@ -0,0 +1,7 @@
+export const getCookie = (name) => {
+        const cookies = document.cookie
+          .split("; ")
+          .find((row) => row.startsWith(`${name}=`));
+       
+        return cookies ? cookies.split("=")[1] : null;
+};
