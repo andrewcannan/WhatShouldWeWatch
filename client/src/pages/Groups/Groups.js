d52ae06b9b9f8e27ee8cb982cb29fe960b0f8af3
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-05 23:32:37 +0100

    add groups page component

---
+++ client/src/pages/Groups/Groups.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GroupsList from '../../components/GroupsList'
+import './Groups.css';
+
+const Groups = () => {
+    return(
+        <div className='groups'>
+            <GroupsList />
+        </div>
+    )
+}
+
+export default Groups;
