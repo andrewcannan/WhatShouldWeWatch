c48d8a73ce45bf363438f34fb4b7adc1742ba849
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-09 23:20:35 +0100

    import LogoNav and render on page

--- client/src/pages/Groups/Groups.js
+++ client/src/pages/Groups/Groups.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GroupsList from '../../components/GroupsList'
+import LogoNav from '../../components/LogoNav';
 import './Groups.css';
 
 const Groups = () => {
     return(
         <div className='groups'>
+            <LogoNav />
             <GroupsList />
         </div>
     )
