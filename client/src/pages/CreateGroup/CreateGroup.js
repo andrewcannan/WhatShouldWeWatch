dc97fed246ec0f761c17fba0beb6113c0f8f0c06
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-24 20:50:46 +0100

    add Create Group page

---
+++ client/src/pages/CreateGroup/CreateGroup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CreateGroupForm from '../../components/forms/CreateGroupForm';
+import LogoNav from '../../components/LogoNav';
+
+
+const CreateGroup = () => {
+    return(
+        <div>
+            <LogoNav />
+            <CreateGroupForm />
+        </div>
+    )
+}
+
+export default CreateGroup;
