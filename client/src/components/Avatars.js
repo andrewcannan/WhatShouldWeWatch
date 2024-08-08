afd57e755e458b67ce022815039da78c618237e8
Andrew Cannan <andrewmcannan@gmail.com>
2024-08-08 20:08:31 +0100

    create reuseable avatars object

---
+++ client/src/components/Avatars.js
@@ -0,0 +1,27 @@
+import avatar1 from '../assets/images/avatar1.jpg';
+import avatar2 from '../assets/images/avatar2.jpg';
+import avatar3 from '../assets/images/avatar3.jpg';
+import avatar4 from '../assets/images/avatar4.jpg';
+import avatar5 from '../assets/images/avatar5.jpg';
+import avatar6 from '../assets/images/avatar6.jpg';
+import avatar7 from '../assets/images/avatar7.jpg';
+import avatar8 from '../assets/images/avatar8.jpg';
+import avatar9 from '../assets/images/avatar9.jpg';
+import createGroup from '../assets/images/createGroup.jpg'
+import joinGroup from '../assets/images/joinGroup.jpg';
+
+const avatars = {
+    avatar1,
+    avatar2,
+    avatar3,
+    avatar4,
+    avatar5,
+    avatar6,
+    avatar7,
+    avatar8,
+    avatar9,
+    createGroup,
+    joinGroup
+};
+
+export default avatars;
