ba3c58d5e5f0bcb24b54c74096f3b5d41119a8c2
Andrew Cannan <andrewmcannan@gmail.com>
2024-08-27 23:02:24 +0100

    add ease-out styles and set timeout for navigation and style adding

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/images/logo.png';
+import { useNavigate } from 'react-router-dom';
+
 import './Welcome.css'
 
 const Welcome = () => {
+    const navigate = useNavigate();
+    const [ isExiting, setIsExiting ] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsExiting(true);
+            setTimeout(() => navigate('/login'), 500)
+        }, 5000);
+
+        return () => clearTimeout(timeout);
+    }, [navigate])
+
     return(
-        <div className='welcome'>
-            <img src={logo} width="150" height="100" className="logo" alt="What Should We Watch Logo"></img>
+        <div className={`welcome ${isExiting? 'exit' : ''}`}>
+            <img src={logo} className="logo" alt="What Should We Watch Logo"></img>
             <h1 className='title'>What Should We Watch</h1>
         </div>
     )
