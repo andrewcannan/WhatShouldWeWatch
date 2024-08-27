fb28364af5f8ba0cadaaa309a18590c947bf2ad8
Andrew Cannan <andrewmcannan@gmail.com>
2024-08-27 19:40:46 +0100

    add styling and title

--- client/src/pages/JoinGroup/JoinGroup.js
+++ client/src/pages/JoinGroup/JoinGroup.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import JoinGroupForm from '../../components/forms/JoinGroupForm';
 import LogoNav from '../../components/LogoNav';
+import './JoinGroup.css'
 
 
 const JoinGroup = () => {
     return(
         <div className='join-group'>
             <LogoNav />
-            <JoinGroupForm />
+            <div className='form-wrapper'>
+                <h1 className='title text-center mb-3'>Join Group</h1>
+                <JoinGroupForm />
+            </div>
         </div>
     )
 }
