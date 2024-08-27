55326513ad6d4ebbe9107dee7771ddf3b71ef5f9
Andrew Cannan <andrewmcannan@gmail.com>
2024-08-27 20:15:26 +0100

    correct setting of formData in handleSearch to remain as an object

--- client/src/components/forms/JoinGroupForm.js
+++ client/src/components/forms/JoinGroupForm.js
@@ -35,7 +35,7 @@ const JoinGroupForm = () => {
                 const data = await response.json();
                 showSuccessToast(data.message);
                 setGroup(data.group);
-                setFormData(data.group.group_code);
+                setFormData({groupCode: data.group.group_code});
             } else {
                 const errorData = await response.json();
                 showErrorToast(errorData.error)
