ca3cece5d6541f46596e09441f5dc696905c1067
Andrew Cannan <andrewmcannan@gmail.com>
2024-09-10 20:04:13 +0100

    add searchform component

---
+++ client/src/components/forms/SearchForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { showSuccessToast, showErrorToast } from '../ToastHelper.js'
+
+
+
+const SearchForm = ({ onSearchResults }) => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState('');
+
+    const handleChange = (e) => {
+        const {id, value} = e.target;
+        setFormData({...formData, [id]: value});
+    };
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`/search_tmdb?groupCode=${formData}`, {
+                method: 'GET',
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                showSuccessToast(data.message);
+                onSearchResults(data.results || []); //pass results back to parent component
+            } else {
+                const errorData = await response.json();
+                showErrorToast(errorData.error)
+            }
+        } catch(error) {
+            showErrorToast('An unexpected error occurred.');
+            console.error(error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSearch}>
+            <div className="form-floating mb-3">
+                <input type="text" className="form-control" id="searchKeyword" onChange={handleChange} required></input>
+                <label htmlFor="searchKeyword">Enter Movie/TV show</label>
+            </div>
+            <div className="d-flex justify-content-between">
+                <button type="submit" className="btn btn-danger">Submit</button>
+                <button type="button" className="btn btn-outline-light" onClick={() => navigate('/')}>Cancel</button>
+            </div>
+        </form>
+    )
+};
+
+export default SearchForm;
