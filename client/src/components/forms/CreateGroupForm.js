eebb309a17c61b9b164c67526ceae3d0bebbdb59
Andrew Cannan <andrewmcannan@gmail.com>
2024-10-01 20:34:36 +0100

    move submit button to right

--- client/src/components/forms/CreateGroupForm.js
+++ client/src/components/forms/CreateGroupForm.js
@@ -76,8 +76,8 @@ const CreateGroupForm = () => {
                     <label htmlFor="groupName">Group Name</label>
                 </div>
                 <div className="d-flex justify-content-between">
-                    <button type="submit" className="btn btn-danger">Create</button>
                     <button type="button" className="btn btn-outline-light" onClick={() => navigate('/groups')}>Cancel</button>
+                    <button type="submit" className="btn btn-danger">Create</button>
                 </div>
             </form>
 
