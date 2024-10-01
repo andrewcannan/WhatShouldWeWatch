360f78f9b1f018dbca3fa578539daad0858d53d0
Andrew Cannan <andrewmcannan@gmail.com>
2024-10-01 20:25:29 +0100

    alter conditional rendering to include searchForm

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -24,21 +24,25 @@ const Search = () => {
     return(
         <div className='search'>
             <LogoNav />
-            <div className='form-wrapper mb-3'>
-                <SearchForm onSearchResults={handleSearchResults} />
-            </div>
-            <div>
-                {!selectedItem && searchResults.map((item) => (
-                    <div className='row mb-3' key={item.id} onClick={() => handleSelectedItem(item)}>
-                        <div className='col-6 col-md-4'>
-                            <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt={item.title || item.name}></img>
+            {!selectedItem && (
+            <>
+                <div className='form-wrapper mb-3'>
+                    <SearchForm onSearchResults={handleSearchResults} />
+                </div>
+                <div>
+                    {searchResults.map((item) => (
+                        <div className='row mb-3' key={item.id} onClick={() => handleSelectedItem(item)}>
+                            <div className='col-6 col-md-4'>
+                                <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt={item.title || item.name}></img>
+                            </div>
+                            <div className='col-6 col-md-8 d-flex justify-content-center align-items-center'>
+                                <p className='text-center'>{item.title || item.name}</p>
+                            </div>
                         </div>
-                        <div className='col-6 col-md-8 d-flex justify-content-center align-items-center'>
-                            <p className='text-center'>{item.title || item.name}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            </>
+            )}
 
             {selectedItem && (
             <ShowDetails item={selectedItem} onCancel={handleCancel} />
