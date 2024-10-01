a9a1ec81f68dccea1b7c5fdb46284b1d382d8bfd
Andrew Cannan <andrewmcannan@gmail.com>
2024-10-01 21:29:18 +0100

    reverse release date, limit rating to 1 decimal, alter layout of elements

--- client/src/components/ShowDetails.js
+++ client/src/components/ShowDetails.js
@@ -2,35 +2,37 @@ import React from "react";
 
 const ShowDetails = ({ item, onCancel}) => {
     return (
-        <div className="show-details">
-            <div className="row">
-                <div className="col-12">
-                    <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt={item.title || item.name}></img>
+        <div className="show-details m-3">
+            <div className="row align-items-center">
+                <div className="col-12 col-md-6 mb-3">
+                    <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title || item.name} className="img-fluid mx-auto d-block"></img>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <p>{item.title || item.name}</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-4">
-                    <p>Released: {item.release_date}</p>
-                </div>
-                <div className="col-4">
-                    <p>Rating: {item.vote_average}/5</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <p>{item.overview}</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <div className="d-flex justify-content-between">
-                        <button type="button" className="btn btn-outline-light" onClick={onCancel}>Cancel</button>
-                        <button type="submit" className="btn btn-danger">Add to List</button>
+                <div className="col-12 col-md-6">
+                    <div className="row">
+                        <div className="col-12">
+                            <p className="display-4">{item.title || item.name}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6 col-md-4">
+                            <p>Released: {item.release_date ? item.release_date.split('-').reverse().join('-') : 'Unknown'}</p>
+                        </div>
+                        <div className="col-6 col-md-4">
+                            <p>Rating: {item.vote_average ? item.vote_average.toFixed(1) : 'No rating available'}/10</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <p>{item.overview}</p>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col-12">
+                            <div className="d-flex justify-content-between">
+                                <button type="button" className="btn btn-outline-light" onClick={onCancel}>Cancel</button>
+                                <button type="submit" className="btn btn-danger">Add to List</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
