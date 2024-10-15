ab1e08656e3f75d5557fdf7e924b3a877c1b16ef
Andrew Cannan <andrewmcannan@gmail.com>
2024-10-15 21:59:59 +0100

    nav-item classes and handle click of search, passing the groupId

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const BottomNav = () => {
+const BottomNav = ({ groupId }) => {
+    const navigate = useNavigate();
+
+    const handleSearchClick = () => {
+        navigate('/search', { state: { groupId } });
+    };
+
+
     return (
        <div className='row fixed-bottom border-top border-secondary text-white text-center' style={{backgroundColor: '#000', fontFamily: 'Roboto'}}>
-            <div className='col-3 border-end border-secondary pt-3'>
+            <div className='col-3 border-end border-secondary pt-3 nav-item' onClick={handleSearchClick}>
                 <i class="fa-solid fa-magnifying-glass"></i>
-                <p>Browse</p>
+                <p>Search</p>
             </div>
-            <div className='col-3 border-end border-secondary pt-3'>
+            <div className='col-3 border-end border-secondary pt-3 nav-item'>
                 <i class="fa-solid fa-film"></i>
                 <p>Movies</p>
             </div>
-            <div className='col-3 border-end border-secondary pt-3'>
+            <div className='col-3 border-end border-secondary pt-3 nav-item'>
                 <i class="fa-solid fa-tv"></i>
                 <p>TV</p>
             </div>
-            <div className='col-3 pt-3'>
+            <div className='col-3 pt-3 nav-item'>
                 <i class="fa-solid fa-gear"></i>
                 <p>Settings</p>
             </div>
