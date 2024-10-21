9836259819fe8cd9a914f16a2191e7795ed66041
Andrew Cannan <andrewmcannan@gmail.com>
2024-10-21 22:11:12 +0100

    change anchor tag to div and handle click event based on if user logged in

--- client/src/components/LogoNav.js
+++ client/src/components/LogoNav.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getSessionCookie } from './CookieUtil';
 import logo from '../assets/images/logo.png'
 
 const LogoNav = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        if (!getSessionCookie()) {
+            navigate('/');
+        }
+        else {
+            navigate('/groups')
+        }
+    };
+
     return (
         <nav className="navbar fixed-top" style={{backgroundColor: '#000', justifyContent: 'center', alignItems: 'center'}}>
-            <a className="navbar-brand d-flex align-items-center" href="/" style={{ fontFamily: 'Montserrat', marginLeft: '5px', color: '#be1423', fontSize: '3vh', fontWeight: '600' }}>
+            <div className="navbar-brand d-flex align-items-center" style={{ fontFamily: 'Montserrat', marginLeft: '5px', color: '#be1423', fontSize: '3vh', fontWeight: '600' }} onClick={handleClick}>
                 <img src={logo} width="60" height="50" className="mr-2" alt="What Should We Watch Logo"></img>
                 What Should We Watch
-            </a>
+            </div>
         </nav>
     )
 }
