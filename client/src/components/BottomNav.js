306ce3094a478b1d035b3cbfc4cb4f8c407ffce4
Andrew Cannan <andrewmcannan@gmail.com>
2024-10-21 22:33:14 +0100

    fix incorrect use of class not className

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -12,19 +12,19 @@ const BottomNav = ({ groupId }) => {
     return (
        <div className='row fixed-bottom border-top border-secondary text-white text-center' style={{backgroundColor: '#000', fontFamily: 'Roboto'}}>
             <div className='col-3 border-end border-secondary pt-3 nav-item' onClick={handleSearchClick}>
-                <i class="fa-solid fa-magnifying-glass"></i>
+                <i className="fa-solid fa-magnifying-glass"></i>
                 <p>Search</p>
             </div>
             <div className='col-3 border-end border-secondary pt-3 nav-item'>
-                <i class="fa-solid fa-film"></i>
+                <i className="fa-solid fa-film"></i>
                 <p>Movies</p>
             </div>
             <div className='col-3 border-end border-secondary pt-3 nav-item'>
-                <i class="fa-solid fa-tv"></i>
+                <i className="fa-solid fa-tv"></i>
                 <p>TV</p>
             </div>
             <div className='col-3 pt-3 nav-item'>
-                <i class="fa-solid fa-gear"></i>
+                <i className="fa-solid fa-gear"></i>
                 <p>Settings</p>
             </div>
        </div>
