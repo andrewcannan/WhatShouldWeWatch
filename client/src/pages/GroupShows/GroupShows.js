9a294028f35e6238eae519c81ec30938b2b0f8a0
Andrew Cannan <andrewmcannan@gmail.com>
2024-11-04 20:20:44 +0000

    add logic to fetch shows and to group returned data by genre

--- client/src/pages/GroupShows/GroupShows.js
+++ client/src/pages/GroupShows/GroupShows.js
@@ -11,6 +11,49 @@ const GroupShows = () => {
     const { groupId } = useParams(); //groupId to be passed to children
     const [ showsByGenre, setShowsByGenre ] = useState({});
 
+    function groupByGenre(data) {
+        const genreMap = {};
+
+        data.forEach(show => {
+            show.genres.forEach(genre => {
+                if (!genreMap[genre]) {
+                    genreMap[genre] = [];
+                }
+            
+            genreMap[genre].push(show);    
+            });
+        });
+
+        return genreMap;
+    };
+
+    useEffect(() => {
+        const getShows = async () => {
+            try {
+                const response = await fetch('/getShows', {
+                    method: 'GET'
+                });
+    
+                if (!response.ok) {
+                    const errorData = await response.json();
+                    showErrorToast(errorData.error)
+                    throw new Error(errorData)
+                }
+    
+                const data = await response.json();
+                const genreData = groupByGenre(data);
+                setShowsByGenre(genreData);
+                
+            }
+            catch (error) {
+                showErrorToast('An unexpected error occurred.');
+                console.error(error);
+            }
+        };
+
+        getShows();
+    }, []);
+
     useEffect(() => {
         if (!getSessionCookie()) {
             showErrorToast("Oops! Please sign in to continue.");
