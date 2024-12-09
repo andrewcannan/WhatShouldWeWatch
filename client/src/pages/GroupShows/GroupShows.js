c9c88a4a521fb91bffe62bd13c93369a74aaa60b
Andrew Cannan <andrewmcannan@gmail.com>
2024-12-09 22:08:06 +0000

    add row for buttons

--- client/src/pages/GroupShows/GroupShows.js
+++ client/src/pages/GroupShows/GroupShows.js
@@ -83,28 +83,37 @@ const GroupShows = () => {
         <div className='group-shows'>
             <LogoNav />
             {!selectedItem && (
-                Object.keys(showsByGenre).filter(genre => showsByGenre[genre].some(show => show.media_type === showType)).map((genre, genreIndex) => (
-                    <div key={genre} className='row genre-row mb-3 no-gutters'>
-                        <p className='h3'>{genre}</p>
-                        <div className='card-deck' ref={(el) => {
-                                    const ref = getRef(genreIndex);
-                                    if (ref) {
-                                        ref.current = el;
-                                    }
-                                }}
-                                onMouseDown={(e) => handleDragStart(genreIndex, e)}
-                                onMouseLeave={() => handleDragEnd(genreIndex)}
-                                onMouseUp={() => handleDragEnd(genreIndex)}
-                                onMouseMove={(e) => handleDrag(genreIndex, e)}>
-                            {showsByGenre[genre].filter(show => show.media_type === showType).map((show, index) => (
-                                <div key={show.id || index} className='card' onClick={() => handleSelectedItem(show)} draggable='false'>
-                                    <img className='card-img-top' src={`https://image.tmdb.org/t/p/w200${show.poster_path}`} alt={show.title || show.name} draggable='false'></img>
-                                    <p className='h5'>{show.title || show.name}</p>
-                                </div>
-                            ))}
+                <>
+                    <div className='row mb-3 no-gutters'>
+                        <div className='d-flex justify-content-around p-3'>
+                            <button type="button" className="btn btn-outline-light" onClick={() => handleShowType('movie')}>Movies</button>
+                            <button type="button" className="btn btn-outline-light" onClick={() => handleShowType('tv')}>TV</button>
                         </div>
                     </div>
-                ))
+
+                    {Object.keys(showsByGenre).filter(genre => showsByGenre[genre].some(show => show.media_type === showType)).map((genre, genreIndex) => (
+                        <div key={genre} className='row genre-row mb-3 no-gutters'>
+                            <p className='h3'>{genre}</p>
+                            <div className='card-deck' ref={(el) => {
+                                        const ref = getRef(genreIndex);
+                                        if (ref) {
+                                            ref.current = el;
+                                        }
+                                    }}
+                                    onMouseDown={(e) => handleDragStart(genreIndex, e)}
+                                    onMouseLeave={() => handleDragEnd(genreIndex)}
+                                    onMouseUp={() => handleDragEnd(genreIndex)}
+                                    onMouseMove={(e) => handleDrag(genreIndex, e)}>
+                                {showsByGenre[genre].filter(show => show.media_type === showType).map((show, index) => (
+                                    <div key={show.id || index} className='card' onClick={() => handleSelectedItem(show)} draggable='false'>
+                                        <img className='card-img-top' src={`https://image.tmdb.org/t/p/w200${show.poster_path}`} alt={show.title || show.name} draggable='false'></img>
+                                        <p className='h5'>{show.title || show.name}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </>
             )};
 
             {selectedItem && (
