462171255810af0edb12edf2873f9d43a6d95cc7
Andrew Cannan <andrewmcannan@gmail.com>
2024-12-29 11:14:22 +0000

    handle click on settings tab

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -12,6 +12,10 @@ const BottomNav = ({ groupId }) => {
         navigate(`/groups/${groupId}`)
     }
 
+    const handleSettingsClick = () => {
+        navigate('/settings', { state: { groupId } });
+    };
+
 
     return (
        <div className='row fixed-bottom border-top border-secondary text-white text-center' style={{backgroundColor: '#000', fontFamily: 'Roboto'}}>
@@ -23,7 +27,7 @@ const BottomNav = ({ groupId }) => {
                 <i className="fa-solid fa-tv"></i>
                 <p>Watch List</p>
             </div>
-            <div className='col-4 pt-3 nav-item'>
+            <div className='col-4 pt-3 nav-item' onClick={handleSettingsClick}>
                 <i className="fa-solid fa-gear"></i>
                 <p>Settings</p>
             </div>
