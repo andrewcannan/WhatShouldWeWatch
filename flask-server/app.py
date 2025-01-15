59759eb96e77cea18f94679dc78cad6d51f1ddd1
Andrew Cannan <andrewmcannan@gmail.com>
2025-01-15 22:51:20 +0000

    configure cookies

--- flask-server/app.py
+++ flask-server/app.py
@@ -13,6 +13,7 @@ CORS(app, resources={r"/*": {"origins": "https://andrewcannan.github.io"}})
 
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
 app.config['SESSION_COOKIE_HTTPONLY'] = False
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 app.config['TMDB_TOKEN'] = os.getenv('TMDB_TOKEN')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
